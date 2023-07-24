d3f557827c2aa941beaadb8084778b5b62575b97
ugurcankuzu <uci2001@hotmail.com>
2023-07-24 09:06:50 +0300

    fix

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfigForDev = {}
-const nextConfigForProd = {
+
+const nextConfig = {
     output: "export",
     basePath: "/personal-page"    
 }
 
-module.exports = process.env.NODE_ENV === "production" ? nextConfigForProd : nextConfigForDev
+module.exports = nextConfig
